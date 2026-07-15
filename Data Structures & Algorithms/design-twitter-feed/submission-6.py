31ebd9630c6aa13756e92ff601a7474b8864fedd
MOHAMED AZARUDEEN <78369373+AzaRKazar@users.noreply.github.com>
2026-07-15 14:33:00 -0400

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.py
@@ -0,0 +1,32 @@
+class Twitter:
+
+    def __init__(self):
+        self.tweetMap=defaultdict(list)
+        self.followMap=defaultdict(set)
+        self.time=0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.time+=1
+        self.tweetMap[userId].append((self.time,tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        maxHeap=[]
+        users={userId} | self.followMap[userId]
+        for i in users:
+            info=self.tweetMap[i][-10:]
+            for t,idx in info:
+                heapq.heappush(maxHeap,(-t,idx))
+        result=[]
+        for i in range(10):
+            if len(maxHeap)==0:
+                return result
+            pop=heapq.heappop(maxHeap)
+            result.append(pop[1])
+        return result
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].add(followeeId)
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
